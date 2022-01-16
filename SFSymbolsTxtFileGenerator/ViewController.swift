f843218e0e6483c4a918db860e763eb6b09227c7
littleossa <littleossa@gmail.com>
2022-01-16 22:39:51 +0900

    [feat] Success to generate SF Symbols.txt to string array

--- SFSymbolsTxtFileGenerator/ViewController.swift
+++ SFSymbolsTxtFileGenerator/ViewController.swift
@@ -11,9 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let sfsymbols = createSFSymbolsClassFile(version: "3_2")
+        print(sfsymbols)
     }
-
-
+    
+    private func createSFSymbolsClassFile(version: String) -> [String] {
+        guard let fileURL = Bundle.main.url(forResource: "SFSymbols" + version, withExtension: "txt"),
+              let fileContents = try? String(contentsOf: fileURL) else {
+                  fatalError("fail to generate")
+              }
+        return fileContents.split(separator: "\n").compactMap { $0.isEmpty ? nil : String($0) }
+    }
 }
-
