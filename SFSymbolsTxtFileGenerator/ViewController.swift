6b689d648721b4885d41a830b1f39a558f6aea1e
littleossa <littleossa@gmail.com>
2022-01-16 22:08:38 +0900

    Initial Commit

---
+++ SFSymbolsTxtFileGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SFSymbolsTxtFileGenerator
+//
+//  Created by 平岡修 on 2022/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
