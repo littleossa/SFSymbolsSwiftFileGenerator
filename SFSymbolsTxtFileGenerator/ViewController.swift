bf11547966b563f2d1493035a76d3fdd7f52bc7a
littleossa <littleossa@gmail.com>
2022-01-17 23:11:26 +0900

    [feat] print SF Symbols enum cases

--- SFSymbolsTxtFileGenerator/ViewController.swift
+++ SFSymbolsTxtFileGenerator/ViewController.swift
@@ -8,19 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let sfsymbols = createSFSymbolsClassFile(version: "3_2")
-        print(sfsymbols)
+        let sfsymbolsNnumCases = createSFSymbolsNnumCases(version: "3_2")
+        let sfSymbolsFile: String = "import Foundation\n\nenum SFSymbol: String, CaseIterable {\n\n%@}"
+        
+        var contents: String = ""
+        for sfsymbolsNnumCase in sfsymbolsNnumCases {
+            contents += sfsymbolsNnumCase + "\n"
+        }
+        let file = String(format: sfSymbolsFile, contents)
+        print(file)
     }
     
-    private func createSFSymbolsClassFile(version: String) -> [String] {
+    private func createSFSymbolsNnumCases(version: String) -> [String] {
         guard let fileURL = Bundle.main.url(forResource: "SFSymbols" + version, withExtension: "txt"),
               let fileContents = try? String(contentsOf: fileURL) else {
                   fatalError("fail to generate")
               }
-        return fileContents.split(separator: "\n").compactMap { $0.isEmpty ? nil : String($0) }
+        return fileContents.split(separator: "\n").compactMap { symbolName in
+            
+            if symbolName.isEmpty { return nil }
+            
+            return "case \(String(symbolName).lowerCamelCase()) = \"\(String(symbolName))\""
+        }
+    }
+}
+
+extension String {
+    
+    func lowerCamelCase() -> String {
+        let separatedCharacters = self.components(separatedBy: ".")
+        var optimalString = ""
+        for i in 0..<separatedCharacters.count {
+            if i == 0 {
+                optimalString = separatedCharacters[i]
+                continue
+            }
+            
+            optimalString += separatedCharacters[i].capitalizeFirstLetter()
+        }
+        return optimalString
+    }
+    
+    private func capitalizeFirstLetter() -> String {
+        
+        return self.prefix(1).uppercased() + self.dropFirst()
     }
 }
